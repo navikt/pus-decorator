6252d0969a2e42e93a004528eeabbecf69458ff1
Håkon Planke Holm <hakon.planke.holm@nav.no>
2018-04-04 16:02:54 +0200

    [FO-367] les både envioremnt variables og system propertys

--- src/main/java/no/nav/pus/decorator/EnvironmentServlet.java
+++ src/main/java/no/nav/pus/decorator/EnvironmentServlet.java
@@ -5,6 +5,8 @@ import javax.servlet.http.HttpServlet;
 import javax.servlet.http.HttpServletRequest;
 import javax.servlet.http.HttpServletResponse;
 import java.io.IOException;
+import java.util.HashMap;
+import java.util.Map;
 
 import static no.nav.pus.decorator.ApplicationConfig.APPLICATION_NAME;
 import static no.nav.sbl.util.EnvironmentUtils.getOptionalProperty;
@@ -13,11 +15,14 @@ import static no.nav.sbl.util.EnvironmentUtils.getOptionalProperty;
 public class EnvironmentServlet extends HttpServlet {
 
     public static final String PUBLIC_PREFIX = "PUBLIC_";
+    public static final String ENVIRONMENT_CONTEXT_PROPERTY_NAME = "ENVIRONMENT_CONTEXT";
     private static final String PUBLIC_PREFIX_PATTERN = "^" + PUBLIC_PREFIX + ".+";
 
-    public static final String ENVIRONMENT_CONTEXT_PROPERTY_NAME = "ENVIRONMENT_CONTEXT";
+    private final String environmentContext = getOptionalProperty(ENVIRONMENT_CONTEXT_PROPERTY_NAME).orElseGet(() -> APPLICATION_NAME);
 
-    private String environmentContext = getOptionalProperty(ENVIRONMENT_CONTEXT_PROPERTY_NAME).orElseGet(() -> APPLICATION_NAME);
+    private static String removePublicPrefix(String string) {
+        return string.replaceFirst(PUBLIC_PREFIX, "");
+    }
 
     @Override
     protected void doGet(HttpServletRequest req, HttpServletResponse resp) throws ServletException, IOException {
@@ -28,24 +33,30 @@ public class EnvironmentServlet extends HttpServlet {
     }
 
     String resolvePublicEnvironment() {
-        String env = System.getProperties()
-                .stringPropertyNames()
+        StringBuilder sb = new StringBuilder();
+
+        getEnvironmentVariablesAndSystemProperties()
+                .entrySet()
                 .stream()
-                .filter(name -> name.matches(PUBLIC_PREFIX_PATTERN))
-                .reduce("", this::toJs);
+                .filter(prop -> prop.getKey().matches(PUBLIC_PREFIX_PATTERN))
+                .map(this::toJs)
+                .forEach(sb::append);
 
-        return environmentContext + "={};\n" + env;
+        return environmentContext + "={};\n" + sb;
     }
 
-    private String toJs(String accumulator, String nextString) {
-        return accumulator + String.format("%s.%s='%s';\n",
+    private Map<String, String> getEnvironmentVariablesAndSystemProperties() {
+        Map<String, String> map = new HashMap<>(System.getenv());
+        System.getProperties().stringPropertyNames().forEach(n -> map.put(n, System.getProperty(n)));
+        return map;
+    }
+
+    private String toJs(Map.Entry<String, String> prop) {
+        return String.format("%s.%s='%s';\n",
                 environmentContext,
-                removePublicPrefix(nextString),
-                System.getProperty(nextString));
-    }
-
-    private static String removePublicPrefix(String string) {
-        return string.replaceFirst(PUBLIC_PREFIX,"");
+                removePublicPrefix(prop.getKey()),
+                prop.getValue()
+        );
     }
 
 }

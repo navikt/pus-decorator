1ea6be4f1d510f501392661b8771e7c005b681b2
Håkon Planke Holm <hakon.planke.holm@nav.no>
2018-10-18 12:28:44 +0200

    [KAIZEN-0] legger til flere CSP regler for hotjar

--- src/main/java/no/nav/pus/decorator/CspService.java
+++ src/main/java/no/nav/pus/decorator/CspService.java
@@ -12,11 +12,11 @@ public class CspService {
     public static String generateCspDirectives() {
         return ""
                 + " default-src 'self' appres.nav.no" + appresTest() + " tjenester.nav.no" + tjenesterTest() + ";"
-                + " script-src 'self' 'unsafe-inline' 'unsafe-eval' appres.nav.no" + appresTest() + " www.googletagmanager.com www.google-analytics.com static.hotjar.com;"
+                + " script-src 'self' 'unsafe-inline' 'unsafe-eval' appres.nav.no" + appresTest() + " www.googletagmanager.com www.google-analytics.com script.hotjar.com static.hotjar.com;"
                 + " img-src 'self' appres.nav.no" + appresTest() + " static.hotjar.com data: ;"
                 + " style-src 'self' 'unsafe-inline' appres.nav.no" + appresTest() + ";"
-                + " font-src 'self' data: ;"
-                + " frame-src video.qbrick.com;" // video i aktivitetsplan, mulig den bør ha spesialisert CSP
+                + " font-src 'self' static.hotjar.com data: ;"
+                + " frame-src vars.hotjar.com video.qbrick.com;" // video i aktivitetsplan, mulig den bør ha spesialisert CSP
                 + " report-uri /frontendlogger/api/warn;";
     }
 

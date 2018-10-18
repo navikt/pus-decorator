996c5023e85a5a2335422abb34b96b235fc0d9ad
Håkon Planke Holm <hakon.planke.holm@nav.no>
2018-10-18 12:41:26 +0200

    [KAIZEN-0] fikser testene

--- src/test/java/no/nav/pus/decorator/CspServiceTest.java
+++ src/test/java/no/nav/pus/decorator/CspServiceTest.java
@@ -13,11 +13,11 @@ public class CspServiceTest {
     public void generateCspDirectives_prod() {
         assertThat(CspService.generateCspDirectives()).isEqualTo("" +
                 " default-src 'self' appres.nav.no tjenester.nav.no;" +
-                " script-src 'self' 'unsafe-inline' 'unsafe-eval' appres.nav.no www.googletagmanager.com www.google-analytics.com static.hotjar.com;" +
+                " script-src 'self' 'unsafe-inline' 'unsafe-eval' appres.nav.no www.googletagmanager.com www.google-analytics.com script.hotjar.com static.hotjar.com;" +
                 " img-src 'self' appres.nav.no static.hotjar.com data: ;" +
                 " style-src 'self' 'unsafe-inline' appres.nav.no;" +
-                " font-src 'self' data: ;" +
-                " frame-src video.qbrick.com;" +
+                " font-src 'self' static.hotjar.com data: ;" +
+                " frame-src vars.hotjar.com video.qbrick.com;" +
                 " report-uri /frontendlogger/api/warn;"
         );
     }
@@ -27,11 +27,11 @@ public class CspServiceTest {
         setTemporaryProperty(FASIT_ENVIRONMENT_NAME_PROPERTY_NAME, "q6", () -> {
             assertThat(CspService.generateCspDirectives()).isEqualTo("" +
                     " default-src 'self' appres.nav.no appres-q6.nav.no tjenester.nav.no tjenester-q6.nav.no;" +
-                    " script-src 'self' 'unsafe-inline' 'unsafe-eval' appres.nav.no appres-q6.nav.no www.googletagmanager.com www.google-analytics.com static.hotjar.com;" +
+                    " script-src 'self' 'unsafe-inline' 'unsafe-eval' appres.nav.no appres-q6.nav.no www.googletagmanager.com www.google-analytics.com script.hotjar.com static.hotjar.com;" +
                     " img-src 'self' appres.nav.no appres-q6.nav.no static.hotjar.com data: ;" +
                     " style-src 'self' 'unsafe-inline' appres.nav.no appres-q6.nav.no;" +
-                    " font-src 'self' data: ;" +
-                    " frame-src video.qbrick.com;" +
+                    " font-src 'self' static.hotjar.com data: ;" +
+                    " frame-src vars.hotjar.com video.qbrick.com;" +
                     " report-uri /frontendlogger/api/warn;"
             );
         });

0d085e66149f1dcad43dc91583f991f8cf036ca3
Alexander Gård <alex.gaard@hotmail.com>
2020-10-21 15:30:44 +0200

    Hent Azure AD B2C config fra nye loginservice idporten env variabler
    Hvis LOGINSERVICE_IDPORTEN_DISCOVERY_URL og LOGINSERVICE_IDPORTEN_AUDIENCE ikke er satt så vil de tidligere AAD_B2C variablene bli brukt.

--- src/main/java/no/nav/pus/decorator/login/LoginServiceResolver.java
+++ src/main/java/no/nav/pus/decorator/login/LoginServiceResolver.java
@@ -1,10 +1,15 @@
 package no.nav.pus.decorator.login;
 
+import no.nav.brukerdialog.security.domain.IdentType;
 import no.nav.brukerdialog.security.oidc.provider.AzureADB2CConfig;
 import no.nav.common.oidc.OidcTokenValidator;
 import no.nav.pus.decorator.config.Config;
+import no.nav.sbl.util.EnvironmentUtils;
 
 import static java.util.Optional.ofNullable;
+import static no.nav.brukerdialog.security.Constants.AZUREADB2C_OIDC_COOKIE_NAME_SBS;
+import static no.nav.brukerdialog.security.oidc.provider.AzureADB2CConfig.EXTERNAL_USERS_AZUREAD_B2C_DISCOVERY_URL;
+import static no.nav.brukerdialog.security.oidc.provider.AzureADB2CConfig.EXTERNAL_USERS_AZUREAD_B2C_EXPECTED_AUDIENCE;
 
 public class LoginServiceResolver {
 
@@ -15,7 +20,15 @@ public class LoginServiceResolver {
     }
 
     private static LoginService oidcLoginService(AuthConfig authConfig, String contextPath) {
-        AzureADB2CConfig azureADB2CConfig = AzureADB2CConfig.configureAzureAdForExternalUsers();
+        String discoveryUrl = EnvironmentUtils.getRequiredProperty("LOGINSERVICE_IDPORTEN_DISCOVERY_URL", EXTERNAL_USERS_AZUREAD_B2C_DISCOVERY_URL);
+        String expectedAudience = EnvironmentUtils.getRequiredProperty("LOGINSERVICE_IDPORTEN_AUDIENCE", EXTERNAL_USERS_AZUREAD_B2C_EXPECTED_AUDIENCE);
+
+        AzureADB2CConfig azureADB2CConfig = AzureADB2CConfig.builder()
+                .discoveryUrl(discoveryUrl)
+                .expectedAudience(expectedAudience)
+                .tokenName(AZUREADB2C_OIDC_COOKIE_NAME_SBS)
+                .identType(IdentType.EksternBruker)
+                .build();
 
         OidcTokenValidator validator = new OidcTokenValidator(azureADB2CConfig.discoveryUrl, azureADB2CConfig.expectedAudience);
         return new OidcLoginService(authConfig, validator, contextPath);

18f62ab031c8a87953589fe10e11b6e073aef54d
Julian Ravn Thrap-Meyer <Julian.Ravn.Thrap-Meyer@nav.no>
2021-07-01 11:33:34 +0200

    Tillat flere subnet-patterns i InternalProtectionFilter

--- src/main/java/no/nav/pus/decorator/security/InternalProtectionFilter.java
+++ src/main/java/no/nav/pus/decorator/security/InternalProtectionFilter.java
@@ -7,8 +7,10 @@ import javax.servlet.http.HttpServletRequest;
 import javax.servlet.http.HttpServletResponse;
 import java.io.IOException;
 import java.util.ArrayList;
+import java.util.Arrays;
 import java.util.List;
 import java.util.regex.Pattern;
+import java.util.stream.Collectors;
 
 import static java.util.Arrays.asList;
 import static javax.servlet.http.HttpServletResponse.SC_FORBIDDEN;
@@ -17,7 +19,10 @@ import static no.nav.apiapp.ApiAppServletContextListener.INTERNAL_IS_ALIVE;
 @Slf4j
 public class InternalProtectionFilter implements Filter {
 
-    private static final Pattern SUBNET_IP_PATTERN = Pattern.compile("192\\.168\\.\\d{1,3}\\.\\d{1,3}");
+    private static final List<Pattern> SUBNET_IP_PATTERNS = Arrays.asList(
+            Pattern.compile("192\\.168\\.\\d{1,3}\\.\\d{1,3}") // on-prem
+    );
+
     private static final List<String> SAFE_POSTFIXES = new ArrayList<>(asList(
             ".oera.no",
             ".oera-q.local",
@@ -51,9 +56,11 @@ public class InternalProtectionFilter implements Filter {
 
     static boolean isAllowedAccessToInternal(String hostname) {
         return "localhost".equals(hostname) // allow for local development and testing
-                || SUBNET_IP_PATTERN.matcher(hostname).matches() // kubernetes uses subnet ips to access internal endpoints
+                || SUBNET_IP_PATTERNS.stream().anyMatch(pattern -> pattern.matcher(hostname).matches()) // kubernetes uses subnet ips to access internal endpoints
                 || SAFE_POSTFIXES.stream().anyMatch(hostname::endsWith) // allow access from internal addresses
                 ;
+
+
     }
 
     @Override
